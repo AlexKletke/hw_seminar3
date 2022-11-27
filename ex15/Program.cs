691ac9687b8efd1a08d6bf0ab0ad34dd13d1f751
AlexandrKletke <klete@mail.ru>
2022-11-27 20:36:15 +0300

    solution of the 19th example

---
+++ ex15/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+using static System.Console;
+WriteLine("enter 5-digit number: ");
+String Number = ReadLine();
+if (Number[0] == Number[4] && Number[1] == Number[3])
+    WriteLine("5-digit number is palindrome");
+else
+    WriteLine("5-digit number is not palindrome");
+
