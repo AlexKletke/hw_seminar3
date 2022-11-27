f6221a741c8130c1f4fff1fc4353fe870c77a133
AlexandrKletke <klete@mail.ru>
2022-11-27 21:35:18 +0300

    solution of 23th example

---
+++ ex23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+using static System.Console;
+WriteLine("enter integer: ");
+int Number = int.Parse(ReadLine());
+for(int i =1; i <= Number; i++)
+{
+    WriteLine($"{i} * {i} * {i} = {i*i*i}");
+}
+WriteLine($"table of cubes of numbers from 1 to {Number} is over");
